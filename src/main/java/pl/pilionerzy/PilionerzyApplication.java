7080b28fdb8ec4f539b54e1f0d59f2e63f26707e
szydra <szymon.draga@gmail.com>
2018-08-23 20:28:29 +0200

    Add the main Spring Boot application class

---
+++ src/main/java/pl/pilionerzy/PilionerzyApplication.java
@@ -0,0 +1,13 @@
+package pl.pilionerzy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilionerzyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PilionerzyApplication.class, args);
+    }
+
+}
