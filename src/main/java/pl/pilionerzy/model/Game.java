4d2d13d9f9e38d940752ee93378aea80beac673a
szydra <szymon.draga@gmail.com>
2018-08-25 11:00:00 +0200

    Add a game model

---
+++ src/main/java/pl/pilionerzy/model/Game.java
@@ -0,0 +1,51 @@
+package pl.pilionerzy.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreationTimestamp
+    private LocalDateTime startTime;
+
+    private Boolean active;
+
+    private Integer level;
+
+    @ManyToMany
+    private Set<Question> askedQuestions;
+
+    private Long lastAskedQuestionId;
+
+    @Override
+    public String toString() {
+        return "Game with id " + id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Game game = (Game) o;
+        return Objects.equals(id, game.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+}
