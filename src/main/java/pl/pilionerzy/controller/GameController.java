396bbbe08a0605ba7c4262ef2e98a62d6a3724ab
szydra <szymon.draga@gmail.com>
2018-08-25 18:03:41 +0200

    Add stop game endpoint.

--- src/main/java/pl/pilionerzy/controller/GameController.java
+++ src/main/java/pl/pilionerzy/controller/GameController.java
@@ -2,9 +2,12 @@ package pl.pilionerzy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.pilionerzy.services.GameService;
+import pl.pilionerzy.model.Game;
+import pl.pilionerzy.model.Question;
+import pl.pilionerzy.service.GameService;
 
 @RestController
 @RequestMapping("/game")
@@ -20,6 +23,17 @@ public class GameController {
     @GetMapping("/start")
     public Long createNewGame() {
         return gameService.startNewGame().getId();
+    }
+
+    @GetMapping("/stop/{gameId}")
+    public Character stopGame(@PathVariable Long gameId) {
+        Game game = gameService.stopGame(gameId);
+        Long questionId = game.getLastAskedQuestionId();
+        return game.getAskedQuestions().stream()
+                .filter(q -> questionId.equals(q.getId()))
+                .map(Question::getCorrectAnswer)
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("Game does not have last asked question"));
     }
 
 }
