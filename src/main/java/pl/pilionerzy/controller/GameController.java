e3ad340445dd31d8099f1161497e03ded3e1ba33
szydra <szymon.draga@gmail.com>
2018-08-25 22:23:18 +0200

    Update game controller.

--- src/main/java/pl/pilionerzy/controller/GameController.java
+++ src/main/java/pl/pilionerzy/controller/GameController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.pilionerzy.model.Game;
-import pl.pilionerzy.model.Question;
 import pl.pilionerzy.service.GameService;
 
 @RestController
@@ -28,12 +27,7 @@ public class GameController {
     @GetMapping("/stop/{gameId}")
     public Character stopGame(@PathVariable Long gameId) {
         Game game = gameService.stopGame(gameId);
-        Long questionId = game.getLastAskedQuestionId();
-        return game.getAskedQuestions().stream()
-                .filter(q -> questionId.equals(q.getId()))
-                .map(Question::getCorrectAnswer)
-                .findAny()
-                .orElseThrow(() -> new IllegalStateException("Game does not have last asked question"));
+        return gameService.getCorrectAnswerPrefix(game);
     }
 
 }
