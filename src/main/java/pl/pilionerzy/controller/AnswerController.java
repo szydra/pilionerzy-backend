3613b5d0b79c73fe55246ba12f94655ffb2c9dad
szydra <szymon.draga@gmail.com>
2018-08-26 10:53:48 +0200

    Add answer controller.

---
+++ src/main/java/pl/pilionerzy/controller/AnswerController.java
@@ -0,0 +1,31 @@
+package pl.pilionerzy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import pl.pilionerzy.service.AnswerService;
+import pl.pilionerzy.util.PrefixUtils;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/answer")
+public class AnswerController {
+
+    private AnswerService answerService;
+
+    @Autowired
+    public AnswerController(AnswerService answerService) {
+        this.answerService = answerService;
+    }
+
+    @PostMapping("/send/{gameId}")
+    public Map<String, String> sendAnswer(@PathVariable Long gameId, @RequestBody Map<String, String> answer) {
+        String rawSelected = answer.get("selected");
+        PrefixUtils.validatePrefix(rawSelected);
+        Character selected = rawSelected.trim().charAt(0);
+        Character correct = answerService.processRequest(gameId, selected);
+        return Collections.singletonMap("prefix", String.valueOf(correct));
+    }
+
+}
