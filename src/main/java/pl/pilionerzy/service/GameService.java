b533b293100ce95376747e46dc69a73712db9d0f
szydra <szymon.draga@gmail.com>
2018-08-26 13:24:28 +0200

    Add game service.

---
+++ src/main/java/pl/pilionerzy/service/GameService.java
@@ -0,0 +1,62 @@
+package pl.pilionerzy.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.pilionerzy.dao.GameDao;
+import pl.pilionerzy.exception.GameException;
+import pl.pilionerzy.exception.NoSuchGameException;
+import pl.pilionerzy.model.Game;
+import pl.pilionerzy.model.Question;
+
+import java.util.Set;
+
+@Service
+public class GameService {
+
+    private GameDao gameDao;
+
+    @Autowired
+    public GameService(GameDao gameDao) {
+        this.gameDao = gameDao;
+    }
+
+    public Game startNewGame() {
+        Game game = new Game();
+        game.setActive(true);
+        game.setLevel(0);
+        return save(game);
+    }
+
+    public Game stopGame(Long gameId) {
+        Game game = findById(gameId);
+        game.setActive(false);
+        return save(game);
+    }
+
+    Game findById(Long gameId) {
+        return gameDao.findById(gameId)
+                .orElseThrow(() -> new NoSuchGameException(gameId));
+    }
+
+    Game save(Game game) {
+        return gameDao.save(game);
+    }
+
+    public Character getCorrectAnswerPrefix(Game game) {
+        Long lastAskedQuestionId = game.getLastAskedQuestionId();
+        return game.getAskedQuestions().stream()
+                .filter(q -> lastAskedQuestionId.equals(q.getId()))
+                .map(Question::getCorrectAnswer)
+                .findAny()
+                .orElseThrow(() -> new GameException("Game does not have last asked question"));
+    }
+
+    void updateLastQuestion(Game game, Question question) {
+        Set<Question> askedQuestions = game.getAskedQuestions();
+        askedQuestions.add(question);
+        game.setAskedQuestions(askedQuestions);
+        game.setLastAskedQuestionId(question.getId());
+        save(game);
+    }
+
+}
