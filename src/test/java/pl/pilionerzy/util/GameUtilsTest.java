5b52f239101e4db05995e308ae0c4032da1b8a4a
szydra <szymon.draga@gmail.com>
2018-11-11 20:39:53 +0100

    AssertJ and new tests for GameUtils.

--- src/test/java/pl/pilionerzy/util/GameUtilsTest.java
+++ src/test/java/pl/pilionerzy/util/GameUtilsTest.java
@@ -5,7 +5,10 @@ import org.junit.Before;
 import org.junit.Test;
 import pl.pilionerzy.exception.GameException;
 import pl.pilionerzy.model.Game;
+import pl.pilionerzy.model.Prefix;
 import pl.pilionerzy.model.Question;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class GameUtilsTest {
 
@@ -16,41 +19,78 @@ public class GameUtilsTest {
         game = new Game();
         Question q1 = new Question();
         q1.setId(1L);
+        q1.setCorrectAnswer(Prefix.A);
         Question q2 = new Question();
         q2.setId(2L);
+        q2.setCorrectAnswer(Prefix.B);
         game.setAskedQuestions(Sets.newHashSet(q1, q2));
     }
 
-    @Test(expected = GameException.class)
+    @Test
     public void testInactiveGame() {
         game.setActive(false);
-        GameUtils.validate(game, RequestType.QUESTION);
-    }
 
-    @Test(expected = GameException.class)
-    public void testTooManyRequestsForAnswer() {
-        game.setLevel(2);
-        GameUtils.validate(game, RequestType.ANSWER);
-    }
+        assertThatExceptionOfType(GameException.class)
+                .isThrownBy(() -> GameUtils.validate(game, RequestType.QUESTION))
+                .withMessageContaining("inactive");
 
-    @Test(expected = GameException.class)
-    public void testTooManyRequestsForQuestion() {
-        game.setLevel(1);
-        GameUtils.validate(game, RequestType.QUESTION);
-    }
-
-    @Test(expected = GameException.class)
-    public void testUnknownRequestType() {
-        game.setActive(true);
-        GameUtils.validate(game, RequestType.UNKNOWN);
     }
 
     @Test
-    public void testValidRequests() {
-        game.setLevel(1);
-        GameUtils.validate(game, RequestType.ANSWER);
+    public void testTooManyRequestsForAnswer() {
         game.setLevel(2);
-        GameUtils.validate(game, RequestType.QUESTION);
+
+        assertThatExceptionOfType(GameException.class)
+                .isThrownBy(() -> GameUtils.validate(game, RequestType.ANSWER));
+    }
+
+    @Test
+    public void testTooManyRequestsForQuestion() {
+        game.setLevel(1);
+
+        assertThatExceptionOfType(GameException.class)
+                .isThrownBy(() -> GameUtils.validate(game, RequestType.QUESTION));
+    }
+
+    @Test
+    public void testUnknownRequestType() {
+        game.setActive(true);
+
+        assertThatExceptionOfType(GameException.class)
+                .isThrownBy(() -> GameUtils.validate(game, RequestType.UNKNOWN));
+    }
+
+    @Test
+    public void testValidRequestForQuestion() {
+        game.setLevel(1);
+
+        assertThatCode(() -> GameUtils.validate(game, RequestType.ANSWER))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void testValidRequestForAnswer() {
+        game.setLevel(2);
+
+        assertThatCode(() -> GameUtils.validate(game, RequestType.QUESTION))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void shouldFindCorrectAnswer() {
+        game.setLastAskedQuestionId(2L);
+
+        Prefix correctAnswerPrefix = GameUtils.getCorrectAnswerPrefix(game);
+
+        assertThat(correctAnswerPrefix).isEqualTo(Prefix.B);
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenLastQuestionIsNotPresent() {
+        game.setLastAskedQuestionId(3L);
+
+        assertThatExceptionOfType(GameException.class)
+                .isThrownBy(() -> GameUtils.getCorrectAnswerPrefix(game));
     }
 
 }
