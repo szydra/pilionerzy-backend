c90d713db1f2b3267f1373ef1759e92fa75d5ecb
szydra <szymon.draga@gmail.com>
2018-11-12 19:29:36 +0100

    Javadoc for exceptions.

--- src/main/java/pl/pilionerzy/exception/GameException.java
+++ src/main/java/pl/pilionerzy/exception/GameException.java
@@ -1,5 +1,8 @@
 package pl.pilionerzy.exception;
 
+/**
+ * This exception is thrown if an existing game cannot be continued for some reason.
+ */
 public class GameException extends RuntimeException {
 
     public GameException(String message) {
--- src/main/java/pl/pilionerzy/exception/NoSuchGameException.java
+++ src/main/java/pl/pilionerzy/exception/NoSuchGameException.java
@@ -1,5 +1,8 @@
 package pl.pilionerzy.exception;
 
+/**
+ * This exception is thrown when a request for a non-existing game is performed.
+ */
 public class NoSuchGameException extends RuntimeException {
 
     public NoSuchGameException(Long gameId) {
