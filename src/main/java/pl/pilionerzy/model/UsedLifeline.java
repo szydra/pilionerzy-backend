eeedda8d77405873baf39d0d1ec48562039caf2a
szydra <szymon.draga@gmail.com>
2019-03-23 15:14:56 +0100

    Used lifeline model added

---
+++ src/main/java/pl/pilionerzy/model/UsedLifeline.java
@@ -0,0 +1,38 @@
+package pl.pilionerzy.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import pl.pilionerzy.model.converter.RejectedAnswersConverter;
+
+import javax.persistence.*;
+import java.util.Collection;
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@Setter
+public class UsedLifeline {
+
+    @Enumerated(EnumType.STRING)
+    private Lifeline type;
+
+    @ManyToOne
+    private Question question;
+
+    @Convert(converter = RejectedAnswersConverter.class)
+    private Collection<Prefix> rejectedAnswers;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UsedLifeline that = (UsedLifeline) o;
+        return type == that.type &&
+                Objects.equals(question, that.question);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(type, question);
+    }
+}
