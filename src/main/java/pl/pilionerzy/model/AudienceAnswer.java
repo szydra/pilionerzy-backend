7e6de003079962f606402ab7385860eb174d80c8
szydra <szymon.draga@gmail.com>
2019-05-05 21:30:27 +0200

    Add the audience answer model

---
+++ src/main/java/pl/pilionerzy/model/AudienceAnswer.java
@@ -0,0 +1,36 @@
+package pl.pilionerzy.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+
+import java.text.DecimalFormat;
+
+@JsonSerialize(using = ToStringSerializer.class)
+public class AudienceAnswer {
+
+    public static final DecimalFormat format;
+
+    static {
+        format = new DecimalFormat("0%");
+        format.setMultiplier(1);
+    }
+
+    private int votes;
+
+    private AudienceAnswer(int votes) {
+        this.votes = votes;
+    }
+
+    public static AudienceAnswer withVotes(int votes) {
+        return new AudienceAnswer(votes);
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+
+    @Override
+    public String toString() {
+        return format.format(votes);
+    }
+}
