d80f925fe3e0bdcdd63652d8ab1d72ce1c5202a0
szydra <szymon.draga@gmail.com>
2019-05-09 21:08:35 +0200

    Add a JUnit test for randomness

--- src/test/java/pl/pilionerzy/util/lifeline/AskTheAudienceCalculatorTest.java
+++ src/test/java/pl/pilionerzy/util/lifeline/AskTheAudienceCalculatorTest.java
@@ -1,7 +1,10 @@
 package pl.pilionerzy.util.lifeline;
 
+import com.google.common.collect.Maps;
+import org.assertj.core.data.Offset;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.internal.util.collections.Sets;
 import pl.pilionerzy.model.AudienceAnswer;
 import pl.pilionerzy.model.Prefix;
 import pl.pilionerzy.model.Question;
@@ -9,6 +12,7 @@ import pl.pilionerzy.util.Unchecker;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static pl.pilionerzy.util.lifeline.AskTheAudienceCalculator.getAnswer;
@@ -58,5 +62,32 @@ public class AskTheAudienceCalculatorTest {
         assertThat(sum)
                 .withFailMessage("Audience answers sum up to %s%% instead of 100%%.", sum)
                 .isEqualTo(100);
+    }
+
+    @Test
+    public void shouldBeDistributedUniformly() {
+        // Given 1000 draws for audience answer
+        int numberOfDraws = 1_000;
+        Map<Prefix, Double> averages = Maps.newHashMap(Maps.asMap(Sets.newSet(Prefix.values()), prefix -> 0.0));
+
+        for (int i = 0; i < numberOfDraws; i++) {
+            Map<Prefix, AudienceAnswer> answer = getAnswer(question, Collections.emptySet());
+            averages.replaceAll((prefix, result) -> result += answer.get(prefix).getVotes());
+        }
+
+        // When calculating the averages
+        averages.replaceAll((prefix, result) -> result / numberOfDraws);
+
+        // The results should be distributed: A approx. 50% and B, C, D approx. 16.6%
+        assertThat(averages.get(Prefix.A))
+                .withFailMessage("Average result for correct answer is expected to be between" +
+                        " 45%% and 55%%, but was %s%%.", averages.get(Prefix.A))
+                .isCloseTo(50.0, Offset.offset(5.0));
+
+        assertThat(Arrays.asList(averages.get(Prefix.B), averages.get(Prefix.C), averages.get(Prefix.D)))
+                .allSatisfy(average -> assertThat(average)
+                        .withFailMessage("Average result for incorrect answer is expected to be between"
+                                + " 11.6%% and 21.6%%, but was %s%%.", average)
+                        .isCloseTo(16.6, Offset.offset(5.0)));
     }
 }
