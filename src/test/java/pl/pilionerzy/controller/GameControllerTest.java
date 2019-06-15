3e9dedd4cae2f6175cd06c765499410d5408bc27
szydra <szymon.draga@gmail.com>
2019-06-15 15:22:48 +0200

    Refactor unit tests for game controller

--- src/test/java/pl/pilionerzy/controller/GameControllerTest.java
+++ src/test/java/pl/pilionerzy/controller/GameControllerTest.java
@@ -5,23 +5,22 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import pl.pilionerzy.model.Game;
+import pl.pilionerzy.dto.GameDto;
 import pl.pilionerzy.model.Prefix;
 import pl.pilionerzy.service.AnswerService;
 import pl.pilionerzy.service.GameService;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.doReturn;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = GameController.class)
-@ComponentScan("pl.pilionerzy.mapping")
 public class GameControllerTest {
 
     @Autowired
@@ -35,40 +34,38 @@ public class GameControllerTest {
 
     @Test
     public void shouldReturnNewGame() throws Exception {
-        Game game = new Game();
+        GameDto game = new GameDto();
         game.setId(1_487L);
         doReturn(game).when(gameService).startNewGame();
 
-        String responseContent = mvc.perform(get("/games/start-new")
+        mvc.perform(get("/games/start-new")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        assertThat(responseContent)
-                .contains("\"id\":1487")
-                .doesNotContain("askedQuestions");
+                .andExpect(jsonPath("$.id").value(1_487L))
+                .andExpect(jsonPath("$.correctAnswer").doesNotExist());
     }
 
     @Test
     public void shouldProcessRequest() throws Exception {
         doReturn(Prefix.C).when(answerService).doAnswer(50L, Prefix.A);
 
-        String responseContent = mvc.perform(post("/games/50/answers")
+        mvc.perform(post("/games/50/answers")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"selected\":\"A\"}"))
                 .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        assertThat(responseContent).contains("\"prefix\":\"C\"");
+                .andExpect(jsonPath("$.prefix").value("C"));
     }
 
     @Test
-    public void shouldStopGameAndReturnCorrectAnswer() throws Exception {
-        doReturn(Prefix.D).when(gameService).stopAndGetCorrectAnswerPrefix(907L);
+    public void shouldReturnStoppedGameWithCorrectAnswer() throws Exception {
+        GameDto game = new GameDto();
+        game.setCorrectAnswer(Prefix.D);
 
-        String responseContent = mvc.perform(put("/games/907/stop")
+        doReturn(game).when(gameService).stopById(907L);
+
+        mvc.perform(post("/games/907/stop")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andReturn().getResponse().getContentAsString();
-        assertThat(responseContent).contains("\"prefix\":\"D\"");
+                .andExpect(jsonPath("$.correctAnswer").value("D"));
     }
-
 }
