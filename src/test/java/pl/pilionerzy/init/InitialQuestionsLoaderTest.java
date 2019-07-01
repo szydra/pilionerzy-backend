812ce922cebbc1b9e853510b93ab160a3ff29929
szydra <szymon.draga@gmail.com>
2019-07-01 20:08:58 +0200

    Add unit tests for initial questions loader

---
+++ src/test/java/pl/pilionerzy/init/InitialQuestionsLoaderTest.java
@@ -0,0 +1,79 @@
+package pl.pilionerzy.init;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import pl.pilionerzy.dao.QuestionDao;
+import pl.pilionerzy.mapping.DtoMapper;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.AdditionalMatchers.find;
+import static org.mockito.AdditionalMatchers.not;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class InitialQuestionsLoaderTest {
+
+    @Mock
+    private Environment environment;
+
+    @Mock
+    private DtoMapper dtoMapper;
+
+    @Mock
+    private Resource loadedResource;
+
+    @Mock
+    private Resource resourceToLoad;
+
+    @Mock
+    private ResourceLoader resourceLoader;
+
+    @Mock
+    private QuestionDao questionDao;
+
+    @InjectMocks
+    private InitialQuestionsLoader questionsLoader;
+
+    @Before
+    public void init() {
+        questionsLoader.setEnvironment(environment);
+        doReturn(resourceToLoad).when(resourceLoader).getResource(not(find("loaded")));
+        doReturn(loadedResource).when(resourceLoader).getResource(find("loaded"));
+        doReturn("questions.json").when(resourceToLoad).getFilename();
+    }
+
+    @Test
+    public void shouldRunWithoutExceptionWhenFileWithQuestionsDoesNotExist() {
+        doReturn(false).when(resourceToLoad).exists();
+
+        assertThatCode(() -> questionsLoader.run()).doesNotThrowAnyException();
+
+        verify(resourceLoader).getResource(isA(String.class));
+        verifyZeroInteractions(dtoMapper, questionDao);
+    }
+
+    @Test
+    public void shouldNotLoadQuestionsWhenFileWithLoadedQuestionsExists() {
+        doReturn(true).when(resourceToLoad).exists();
+        doReturn(true).when(loadedResource).exists();
+
+        assertThatCode(() -> questionsLoader.run()).doesNotThrowAnyException();
+
+        verify(resourceLoader, times(2)).getResource(isA(String.class));
+        verifyZeroInteractions(dtoMapper, questionDao);
+    }
+
+    @Test
+    public void shouldPrepareChangedName() {
+        assertThat(questionsLoader.getChangedName("questions.json"))
+                .isEqualTo("questions_loaded.json");
+    }
+}
