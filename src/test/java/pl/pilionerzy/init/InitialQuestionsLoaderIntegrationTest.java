f7c2e0ed1a6df3385f4ec05e0b73401ff9c5791c
szydra <szymon.draga@gmail.com>
2019-07-01 20:42:48 +0200

    Add an integration test for initial questions loader

---
+++ src/test/java/pl/pilionerzy/init/InitialQuestionsLoaderIntegrationTest.java
@@ -0,0 +1,29 @@
+package pl.pilionerzy.init;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.pilionerzy.dao.QuestionDao;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class InitialQuestionsLoaderIntegrationTest {
+
+    @Autowired
+    private QuestionDao questionDao;
+
+    @Test
+    public void shouldLoadInitialQuestionsAndActivateThem() {
+        assertThat(questionDao.findAll())
+                .hasSize(5)
+                .allSatisfy(question ->
+                        assertThat(question.getActive()).isTrue()
+                );
+    }
+}
