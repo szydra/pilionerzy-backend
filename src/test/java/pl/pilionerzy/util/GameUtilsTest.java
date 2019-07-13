1c878307b1f7d31acb000ee87c7253a1a12efe72
szydra <szymon.draga@gmail.com>
2019-07-13 18:47:10 +0200

    Add new unit tests for game utils

--- src/test/java/pl/pilionerzy/util/GameUtilsTest.java
+++ src/test/java/pl/pilionerzy/util/GameUtilsTest.java
@@ -1,14 +1,20 @@
 package pl.pilionerzy.util;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
 import org.junit.Test;
 import pl.pilionerzy.exception.GameException;
-import pl.pilionerzy.model.Game;
-import pl.pilionerzy.model.Prefix;
-import pl.pilionerzy.model.Question;
+import pl.pilionerzy.model.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
-import static pl.pilionerzy.util.GameUtils.validate;
+import static pl.pilionerzy.model.Lifeline.*;
+import static pl.pilionerzy.model.Prefix.A;
+import static pl.pilionerzy.model.Prefix.C;
+import static pl.pilionerzy.util.GameUtils.*;
 import static pl.pilionerzy.util.RequestType.*;
 
 public class GameUtilsTest {
@@ -18,12 +24,23 @@ public class GameUtilsTest {
     private void prepareQuestions() {
         Question q1 = new Question();
         q1.setId(1L);
-        q1.setCorrectAnswer(Prefix.A);
+        q1.setCorrectAnswer(A);
         Question q2 = new Question();
         q2.setId(2L);
         q2.setCorrectAnswer(Prefix.B);
         game.setAskedQuestions(Sets.newHashSet(q1, q2));
         game.setLastAskedQuestion(q2);
+    }
+
+    private void prepareLifelines(Lifeline... lifelines) {
+        List<UsedLifeline> usedLifelines = new ArrayList<>();
+        Arrays.stream(lifelines)
+                .forEach(lifeline -> {
+                    UsedLifeline usedLifeline = new UsedLifeline();
+                    usedLifeline.setType(lifeline);
+                    usedLifelines.add(usedLifeline);
+                });
+        game.setUsedLifelines(usedLifelines);
     }
 
     @Test
@@ -93,5 +110,51 @@ public class GameUtilsTest {
         assertThatExceptionOfType(GameException.class)
                 .isThrownBy(() -> validate(game, LIFELINE))
                 .withMessage("Cannot use a lifeline for a game without last asked question");
+    }
+
+    @Test
+    public void shouldWorkCorrectlyWhenNoLifelineWasUsed() {
+        prepareQuestions();
+        prepareLifelines();
+
+        assertThat(isLifelineUsed(game, FIFTY_FIFTY)).isFalse();
+    }
+
+    @Test
+    public void shouldWorkCorrectlyWhenRequestedLifelineWasUsed() {
+        prepareQuestions();
+        prepareLifelines(PHONE_A_FRIEND);
+
+        assertThat(isLifelineUsed(game, PHONE_A_FRIEND)).isTrue();
+    }
+
+    @Test
+    public void shouldWorkCorrectlyWhenRequestedLifelineWasNotUsed() {
+        prepareQuestions();
+        prepareLifelines(FIFTY_FIFTY, PHONE_A_FRIEND);
+
+        assertThat(isLifelineUsed(game, ASK_THE_AUDIENCE)).isFalse();
+    }
+
+    @Test
+    public void shouldReturnEmptySetWhenNoLifelineWasUsed() {
+        prepareQuestions();
+        prepareLifelines();
+
+        assertThat(getRejectedAnswers(game)).isEmpty();
+    }
+
+    @Test
+    public void shouldReturnRejectedPrefixesWhenFiftyWasUsed() {
+        prepareQuestions();
+        prepareLifelines(FIFTY_FIFTY);
+        game.getUsedLifelines().stream()
+                .filter(lifeline -> lifeline.getType() == FIFTY_FIFTY)
+                .forEach(lifeline -> {
+                    lifeline.setQuestion(game.getLastAskedQuestion());
+                    lifeline.setRejectedAnswers(ImmutableSet.of(A, C));
+                });
+
+        assertThat(getRejectedAnswers(game)).containsExactlyInAnyOrder(A, C);
     }
 }
