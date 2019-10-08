7c7c4e843ff0540fa3f091c7094f8f2c495f0f55
szydra <szymon.draga@gmail.com>
2019-10-08 18:29:08 +0200

    Add unit tests for question entity

--- src/test/java/pl/pilionerzy/model/QuestionTest.java
+++ src/test/java/pl/pilionerzy/model/QuestionTest.java
@@ -26,4 +26,47 @@ public class QuestionTest {
                 .isThrownBy(() -> question.activate())
                 .withMessage("Active question cannot be activated");
     }
+
+    @Test
+    public void shouldDeactivateActiveQuestion() {
+        question.setActive(true);
+
+        question.deactivate();
+
+        assertThat(question.getActive()).isFalse();
+    }
+
+    @Test
+    public void shouldThrowExceptionWhenDeactivatingInactiveQuestion() {
+        question.setActive(false);
+
+        assertThatIllegalStateException()
+                .isThrownBy(() -> question.deactivate())
+                .withMessage("Inactive question cannot be deactivated");
+    }
+
+    @Test
+    public void questionsWithTheSameBusinessIdShouldBeEqual() {
+        String businessId = "1a2b3c4c5e6f7g8h";
+        question.setBusinessId(businessId);
+        question.setId(1L);
+        question.setContent("content 1");
+
+        Question otherQuestion = new Question();
+        otherQuestion.setBusinessId(businessId);
+        otherQuestion.setId(2L);
+        otherQuestion.setContent("content 2");
+
+        assertThat(question).isEqualTo(otherQuestion);
+    }
+
+    @Test
+    public void questionsWithDistinctBusinessIdsShouldNotBeEqual() {
+        question.setBusinessId("1a2b3c4c5e6f7g8h");
+
+        Question otherQuestion = new Question();
+        otherQuestion.setBusinessId("8h1a2b3c4c5e6f7g");
+
+        assertThat(question).isNotEqualTo(otherQuestion);
+    }
 }
