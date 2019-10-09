83b29c1d40162be08b3beecacd0906dd4b6f47ba
szydra <szymon.draga@gmail.com>
2019-10-09 20:19:32 +0200

    Add custom game assert

--- src/test/java/pl/pilionerzy/assertion/Assertions.java
+++ src/test/java/pl/pilionerzy/assertion/Assertions.java
@@ -1,10 +1,16 @@
 package pl.pilionerzy.assertion;
 
+import pl.pilionerzy.model.Game;
+
 import javax.validation.ConstraintViolationException;
 
 public class Assertions extends org.assertj.core.api.Assertions {
 
     public static ConstraintViolationExceptionAssert assertThat(ConstraintViolationException actual) {
         return new ConstraintViolationExceptionAssert(actual);
+    }
+
+    public static GameAssert assertThat(Game actual) {
+        return new GameAssert(actual);
     }
 }
---
+++ src/test/java/pl/pilionerzy/assertion/GameAssert.java
@@ -0,0 +1,47 @@
+package pl.pilionerzy.assertion;
+
+import org.assertj.core.api.AbstractObjectAssert;
+import pl.pilionerzy.model.Game;
+
+import java.util.Objects;
+
+import static java.lang.Boolean.FALSE;
+import static java.lang.Boolean.TRUE;
+
+public class GameAssert extends AbstractObjectAssert<GameAssert, Game> {
+
+    GameAssert(Game actual) {
+        super(actual, GameAssert.class);
+    }
+
+    public GameAssert isActive() {
+        isNotNull();
+
+        if (!TRUE.equals(actual.getActive())) {
+            failWithMessage("Expected to be active, but 'active' field was <%s>", actual.getActive());
+        }
+
+        return this;
+    }
+
+    public GameAssert isInactive() {
+        isNotNull();
+
+        if (!FALSE.equals(actual.getActive())) {
+            failWithMessage("Expected to be inactive, but 'active' field was <%s>", actual.getActive());
+        }
+
+        return this;
+    }
+
+    public GameAssert hasLevel(int expectedLevel) {
+        isNotNull();
+
+        Integer actualLevel = actual.getLevel();
+        if (!Objects.equals(actualLevel, expectedLevel)) {
+            failWithMessage("Expected to have level <%s>, but was <%s>", expectedLevel, actualLevel);
+        }
+
+        return this;
+    }
+}
