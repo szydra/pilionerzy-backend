b3f647ba76213416a4d55a04a82b6ee5338c48e3
szydra <szymon.draga@gmail.com>
2022-06-10 20:26:25 +0200

    Create level service

--- src/main/java/pl/pilionerzy/util/LevelUtils.java
+++ src/main/java/pl/pilionerzy/util/LevelUtils.java
@@ -1,27 +1,8 @@
 package pl.pilionerzy.util;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class LevelUtils {
 
     private static final int HIGHEST_LEVEL = 12;
-
-    private static final List<Integer> GUARANTEED_LEVELS = Arrays.asList(0, 2, 7, 12);
-
-    public static int getNextLevel(int currentLevel) {
-        return currentLevel + 1;
-    }
-
-    public static int getGuaranteedLevel(int currentLevel) {
-        if (currentLevel < 0 || currentLevel > 12) {
-            throw new IllegalArgumentException("Game level has to be between 0 and 12");
-        }
-        while (!GUARANTEED_LEVELS.contains(currentLevel)) {
-            currentLevel--;
-        }
-        return currentLevel;
-    }
 
     public static boolean isHighestLevel(int currentLevel) {
         return currentLevel == HIGHEST_LEVEL;
---
+++ src/test/java/pl/pilionerzy/service/LevelServiceTest.java
@@ -0,0 +1,95 @@
+package pl.pilionerzy.service;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import pl.pilionerzy.model.Level;
+import pl.pilionerzy.repository.LevelRepository;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+
+@RunWith(MockitoJUnitRunner.class)
+public class LevelServiceTest {
+
+    @Mock
+    private LevelRepository levelRepository;
+
+    @InjectMocks
+    private LevelService levelService;
+
+    @Before
+    public void setLevels() {
+        doReturn(mockLevels()).when(levelRepository).findByOrderByIdAsc();
+    }
+
+    @Test
+    public void testNegativeLevel() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> levelService.getGuaranteedLevel(-5));
+    }
+
+    @Test
+    public void testTooLargeLevel() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> levelService.getGuaranteedLevel(13));
+    }
+
+    @Test
+    public void testLevel10() {
+        int guaranteedLevel = levelService.getGuaranteedLevel(10);
+        assertThat(guaranteedLevel).isEqualTo(7);
+    }
+
+    @Test
+    public void testLevel7() {
+        int guaranteedLevel = levelService.getGuaranteedLevel(7);
+        assertThat(guaranteedLevel).isEqualTo(7);
+    }
+
+    @Test
+    public void testLevel1() {
+        int guaranteedLevel = levelService.getGuaranteedLevel(1);
+        assertThat(guaranteedLevel).isZero();
+    }
+
+    @Test
+    public void testNoGuaranteedLevel() {
+        doReturn(List.of(level(0, false), level(1, false)))
+                .when(levelRepository)
+                .findByOrderByIdAsc();
+
+        assertThatIllegalStateException().isThrownBy(() -> levelService.getGuaranteedLevel(1));
+    }
+
+    private List<Level> mockLevels() {
+        return List.of(
+                level(0, true),
+                level(1, false),
+                level(2, true),
+                level(3, false),
+                level(4, false),
+                level(5, false),
+                level(6, false),
+                level(7, true),
+                level(8, false),
+                level(9, false),
+                level(10, false),
+                level(11, false),
+                level(12, true)
+        );
+    }
+
+    private Level level(Integer id, Boolean guaranteed) {
+        var level = new Level();
+        level.setId(id);
+        level.setAward(id.toString());
+        level.setGuaranteed(guaranteed);
+        return level;
+    }
+}
