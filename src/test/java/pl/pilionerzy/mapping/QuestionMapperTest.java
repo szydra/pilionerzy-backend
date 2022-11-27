1c8ac18a7a5d41eb8757f5905ea09c4e24aab83a
szydra <szymon.draga@gmail.com>
2022-11-27 17:48:51 +0100

    Adjust question mapper test

--- src/test/java/pl/pilionerzy/mapping/QuestionMapperTest.java
+++ src/test/java/pl/pilionerzy/mapping/QuestionMapperTest.java
@@ -1,14 +1,13 @@
 package pl.pilionerzy.mapping;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import pl.pilionerzy.dto.NewAnswerDto;
 import pl.pilionerzy.dto.NewQuestionDto;
 import pl.pilionerzy.model.Prefix;
-import pl.pilionerzy.model.Question;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,49 +15,58 @@ import java.util.List;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static pl.pilionerzy.assertion.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
-@ContextConfiguration(classes = {AnswerMapperImpl.class, QuestionMapperImpl.class})
-public class QuestionMapperTest {
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {QuestionMapperImpl.class})
+class QuestionMapperTest {
 
     @Autowired
     private QuestionMapper questionMapper;
 
     @Test
-    public void shouldCalculateHash() {
-        NewQuestionDto questionDto = createNewQuestionDto();
+    void shouldCalculateHash() {
+        // given
+        var questionDto = createNewQuestionDto();
 
-        Question question = questionMapper.dtoToModel(questionDto, new LoopAvoidingContext());
+        // when
+        var question = questionMapper.dtoToModel(questionDto, new LoopAvoidingContext());
 
-        String hash = questionMapper.calculateHash(questionDto);
+        // then
+        var hash = questionMapper.calculateHash(questionDto);
         assertThat(question.getHash()).isEqualTo(hash).hasSize(32);
     }
 
     @Test
-    public void shouldNotActivateNewQuestion() {
-        NewQuestionDto questionDto = createNewQuestionDto();
+    void shouldNotActivateNewQuestion() {
+        // given
+        var questionDto = createNewQuestionDto();
 
-        Question question = questionMapper.dtoToModel(questionDto, new LoopAvoidingContext());
+        // when
+        var question = questionMapper.dtoToModel(questionDto, new LoopAvoidingContext());
 
+        // then
         assertThat(question.getActive()).isFalse();
     }
 
     @Test
-    public void shouldSetQuestionInAllAnswers() {
-        NewQuestionDto questionDto = createNewQuestionDto();
+    void shouldSetQuestionInAllAnswers() {
+        // given
+        var questionDto = createNewQuestionDto();
 
-        Question question = questionMapper.dtoToModel(questionDto, new LoopAvoidingContext());
+        // when
+        var question = questionMapper.dtoToModel(questionDto, new LoopAvoidingContext());
 
+        // then
         assertThat(question.getAnswers())
                 .hasSize(4)
                 .allMatch(answer -> answer.getQuestion() == question);
     }
 
     private NewQuestionDto createNewQuestionDto() {
-        NewQuestionDto questionDto = new NewQuestionDto();
+        var questionDto = new NewQuestionDto();
         questionDto.setContent(randomAlphanumeric(32));
         List<NewAnswerDto> answers = new ArrayList<>(4);
         for (Prefix prefix : Prefix.values()) {
-            NewAnswerDto answer = new NewAnswerDto();
+            var answer = new NewAnswerDto();
             answer.setPrefix(prefix);
             answer.setContent(randomAlphanumeric(32));
             answer.setQuestion(questionDto);
